0e68b17fa035f6fa348d0c406041c3fcd99f0b09
valeredunand <valere.dunand@dpi.fr>
2019-05-28 00:16:02 +0200

    Use Map in Evt

--- js/oev/utils/event.js
+++ js/oev/utils/event.js
@@ -1,24 +1,26 @@
 class Evt {
 
 	constructor() {
-		this.events = {};
-		this.listeners = {};
+		this.events = new Map();
+		this.listeners = new Map();
 	}
 
 	addEventListener(_evtName, _listener, _callback) {
-		if (this.events[_evtName] === undefined) {
-			this.events[_evtName] = [];
-			this.listeners[_evtName] = [];
+		if (!this.events.has(_evtName)) {
+			this.events.set(_evtName, []);
+			this.listeners.set(_evtName, []);
 		}
-		this.events[_evtName].push(_callback);
-		this.listeners[_evtName].push(_listener);
+		this.events.get(_evtName).push(_callback);
+		this.listeners.get(_evtName).push(_listener);
 	}
 
 	removeEventListener(_evtName, _listener, _callback) {
 		var index = -1;
-		if (!this.events.hasOwnProperty(_evtName)) return false;
-		for (let i = 0; i < this.listeners[_evtName].length; i ++) {
-			if (this.listeners[_evtName][i] == _listener && this.events[_evtName][i] == _callback) {
+		if (!this.events.has(_evtName)) return false;
+		const listeners = this.listeners.get(_evtName);
+		const events = this.events.get(_evtName);
+		for (let i = 0; i < listeners.length; i ++) {
+			if (listeners[i] == _listener && events[i] == _callback) {
 				index = i;
 				break;
 			}
@@ -27,14 +29,14 @@ class Evt {
 			// console.error('removeEventListener "' + _evtName + '" NOT found');
 			return false;
 		}
-		this.events[_evtName].splice(index, 1);
-		this.listeners[_evtName].splice(index, 1);
+		this.events.get(_evtName).splice(index, 1);
+		listeners.splice(index, 1);
 	}
 
 	fireEvent(_evtName, _args = []) {
-		if (!this.events.hasOwnProperty(_evtName)) return false;
-		var evs = this.events[_evtName].slice(0);
-		var lst = this.listeners[_evtName].slice(0);
+		if (!this.events.has(_evtName)) return false;
+		var evs = this.events.get(_evtName).slice(0);
+		var lst = this.listeners.get(_evtName).slice(0);
 		var listenerNb = evs.length;
 		for (let i = 0; i < listenerNb; i++) {
 			evs[i].call(lst[i], _args);
